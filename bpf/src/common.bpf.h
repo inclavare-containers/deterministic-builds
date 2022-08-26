463b1f2f12bc847363cd8e3deb2a5e7dedf7dee4
Zhang Xingjian <xingjian.zhang@qq.com>
2022-08-26 20:31:48 +0800

    [feat] Add modify random

--- bpf/src/common.bpf.h
+++ bpf/src/common.bpf.h
@@ -1,7 +1,21 @@
 
-bool comm_filter(char *comm) {
-  if (!(__builtin_memcmp("cc1\0", comm, 4) == 0)) {
-    return 0;
+__inline__ __attribute__((always_inline)) bool comm_filter(char *comm) {
+  if ((__builtin_memcmp("cc1\0", comm, 4) == 0)) {
+    return 1;
   }
-  return 1;
+  if ((__builtin_memcmp("cc1p", comm, 4) == 0) &&
+      (__builtin_memcmp("lus\0", comm + 4, 4) == 0)) {
+    return 1;
+  }
+  if ((__builtin_memcmp("lto1", comm, 4) == 0) &&
+      (__builtin_memcmp("-wpa", comm + 4, 4) == 0)) {
+    return 1;
+  }
+  if ((__builtin_memcmp("g++\0", comm, 4) == 0)) {
+    return 1;
+  }
+  if ((__builtin_memcmp("gcc\0", comm, 4) == 0)) {
+    return 1;
+  }
+  return 0;
 }
